78ce015b62723fc97fd636d31a27f5c51e45bcd1
zhu.qi <zhu.qi@kandoudou.com.cn>
2020-05-25 17:59:31 +0800

    css盒子模型居中

--- prototype.js
+++ prototype.js
@@ -8,3 +8,12 @@ function func(name){
 var f=new func('phoebe'); // f是func实例化之后的一个构造函数，new是用来实例化函数的一种手段。
 // 而func的构造函数实际上是js内置的function，实际上 function func(name){}等价于 var func = function(name){}
 f.sayHello();
+
+
+function Person() {
+
+}
+var person = new Person(); // Person 就是一个构造函数，我们使用 new 创建了一个实例对象 person
+person.name = 'Kevin';
+console.log(person.name) // Kevin
+
